8bc60fe0e1ff8edbdf774f0e5f32c5169596d00d
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-01 15:54:08 -0400

    Update Core.c

--- Core.c
+++ Core.c
@@ -34,7 +34,7 @@ bool tickFunc(Core *core)
 	//** instruction fetch **
 	// Steps may include
     // (Step 1) Reading instruction from instruction memory
-    unsigned instruction = core->instr_mem->instructions[core->PC / 4].instruction;
+   /*  unsigned instruction = core->instr_mem->instructions[core->PC / 4].instruction;
 	Signal incremented_instruction = core->PC += 4;	
 	printf("inside tick function ");
 	//** decoding / reg reading  **
@@ -68,7 +68,7 @@ bool tickFunc(Core *core)
 		core->reg_file[reg_index_1] = 0;// result of memory manipulation Mux all the way to the right
 	
 	}
-/* 
+
 	//call 
 	// ** execute / address calc
 	// mux1
@@ -100,8 +100,8 @@ bool tickFunc(Core *core)
 	core->reg_file[write_register] = mux_3_signal;
 	incremented_instruction = core->PC = mux_3_signal;
 	//printf("The data in register %x is %lx",write_register, core->reg_file[write_register]);
- 
   */
+  
     ++core->clk;
     // Are we reaching the final instruction?
     if (core->PC > core->instr_mem->last->addr)
