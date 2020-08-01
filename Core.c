d05134962546c10154e8eab1ce9844c41e1439b2
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-01 13:41:46 -0400

    Update Core.c

--- Core.c
+++ Core.c
@@ -1,4 +1,5 @@
 #include "Core.h"
+#include "Registers.h"
 
 Core *initCore(Instruction_Memory *i_mem)
 {
@@ -6,31 +7,97 @@ Core *initCore(Instruction_Memory *i_mem)
     core->clk = 0;
     core->PC = 0;
     core->instr_mem = i_mem;
-    printf("in initcore. before initfunctcall\n");
     core->tick = tickFunc;
 
     // FIXME, initialize register file here.
     // core->data_mem[0] = ...
+	core -> data_mem[0] = 16;
+	core -> data_mem[1] = 128;
+	core -> data_mem[2] = 8;
+	core -> data_mem[3] = 4;
 
     // FIXME, initialize data memory here.
-    // core->reg_file[0] = ...
+    core->reg_file[25] = 4;
+	core->reg_file[10] = 4;
+	core->reg_file[22] = 1;
 
     return core;
 }
 
 // FIXME, implement this function
 bool tickFunc(Core *core)
-{
-    printf("just started tick func\n");
-    // Steps may include
+{           
+
+	// *********** organized by processing stages (lecture notes #9 )****************
+	
+	//** instruction fetch **
+	// Steps may include
     // (Step 1) Reading instruction from instruction memory
     unsigned instruction = core->instr_mem->instructions[core->PC / 4].instruction;
-    printf("the instruction in tickfunc is %u\n", instruction);
-    // (Step 2) ...
+	Signal incremented_instruction = core->PC += 4;	
+	printf("inside tick function ");
+	//** decoding / reg reading  **
+	
+	// call control unit 
+	/* Signal control_unit_input = (instruction / 64);
+	ControlSignals *signals = NULL;
+	ControlUnit(control_unit_input, signals);
+	// run immGen 
+	Signal ImmeGen_sig = ImmeGen(instruction); // <------------------------------------ not finished, fix this!!!!!!!!!!!!!
+	
+	//get reg values
+	
+	// get inputs for reg file from instructions
+	int reg_index_1,reg_index_2,write_register;
+	reg_index_1 = (instruction / 524288)>>15;
+	reg_index_2 = (instruction / 16777216)>>20;
+	write_register = (instruction / 2048)>>7;
+	Signal reg_read_1 = 0;
+	Signal reg_read_2 = 0;
+	if ( signals->RegWrite== 0 )
+	{
+		reg_read_1 = core->reg_file[reg_index_1];
+		reg_read_2 = core->reg_file[reg_index_2];
+	}
+	else if (signals->RegWrite == 1)		
+	{
+		core->reg_file[reg_index_1] = 0;// result of memory manipulation Mux all the way to the right
+	
+	}
+	
+	//call 
+	// ** execute / address calc
+	// mux1
+	Signal mux_1_signal = MUX( signals->ALUSrc, reg_read_2,ImmeGen_sig);
+			
+	
+	
+	//Alu control 
+	Signal aluControlResult = ALUControlUnit(signals->ALUOp, instruction>>24,instruction >> 11);
+	// alu 	
+	Signal *ALU_result = NULL;
+	Signal *zero = NULL;
+	ALU(reg_read_1,mux_1_signal,aluControlResult, ALU_result, zero);
+	
+	Signal branch_location = Add(incremented_instruction,aluControlResult<<1);
+	//** memory access 
+	
+	Signal memory_result ;
+	memory_result = 0; // <----------------------- will fix when  dealing with ld 
+	Signal mux_2_signal = MUX(signals->MemtoReg, *ALU_result, memory_result); // fix this 
+	// <-------------------- figure out how i type loads results in  register 
+	
+	
+	
+	// mux 3
+	Signal mux_3_control = *zero && signals->Branch ;
+	Signal mux_3_signal = MUX( mux_3_control,incremented_instruction,branch_location);
+	//write results
+	core->reg_file[write_register] = mux_3_signal;
+	incremented_instruction = core->PC = mux_3_signal;
+	//printf("The data in register %x is %lx",write_register, core->reg_file[write_register]);
     
-    // (Step N) Increment PC. FIXME, is it correct to always increment PC by 4?!
-    core->PC += 4;
-
+ */
     ++core->clk;
     // Are we reaching the final instruction?
     if (core->PC > core->instr_mem->last->addr)
@@ -55,6 +122,41 @@ void ControlUnit(Signal input,
         signals->Branch = 0;
         signals->ALUOp = 2;
     }
+	// For I-type -- ld
+    if (input == 3)
+    {
+        signals->ALUSrc = 1;
+        signals->MemtoReg = 1;
+        signals->RegWrite = 1;
+        signals->MemRead = 1;
+        signals->MemWrite = 0;
+        signals->Branch = 0;
+        signals->ALUOp = 3;
+    }
+	
+	// For I-type -- slli <------------------ fix this
+    if (input == 19)
+    {
+        signals->ALUSrc = 1;
+        signals->MemtoReg = 0;
+        signals->RegWrite = 1;
+        signals->MemRead = 1;
+        signals->MemWrite = 0;
+        signals->Branch = 0;
+        signals->ALUOp = 3;
+    }
+	
+	// For sb-type -- bne <--------------------- fix this
+    if (input == 99)
+    {
+        signals->ALUSrc = 0;
+        signals->MemtoReg = 0;
+        signals->RegWrite = 0;
+        signals->MemRead = 0;
+        signals->MemWrite = 0;
+        signals->Branch = 1;
+        signals->ALUOp = 0;
+    }
 }
 
 // FIXME (2). ALU Control Unit. Refer to Figure 4.12.
@@ -67,12 +169,18 @@ Signal ALUControlUnit(Signal ALUOp,
     {
         return 2;
     }
+	// For ldd
+    if (ALUOp == 2 && Funct3 == 7)
+    {
+        return 3;
+    }
+	
 }
 
 // FIXME (3). Imme. Generator
 Signal ImmeGen(Signal input)
 {
-
+	long ImmeGen = input;
 }
 
 // FIXME (4). ALU
@@ -88,6 +196,13 @@ void ALU(Signal input_0,
         *ALU_result = (input_0 + input_1);
         if (*ALU_result == 0) { *zero = 1; } else { *zero = 0; }
     }
+	// For ld
+    if (ALU_ctrl_signal == 3)
+    {
+        *ALU_result = input_1;
+        if (*ALU_result == 0) { *zero = 1; } else { *zero = 0; }
+    }
+	
 }
 
 // (4). MUX
